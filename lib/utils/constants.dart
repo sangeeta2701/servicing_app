95feebba9ced6822dc1fdf44e40092877ddce14d
DELL <anjugupta7060@gmail.com>
2023-02-28 20:04:09 +0530

    Car Servicing

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appUiDarkColor = Colors.black;
+const Color appUiLightColor = Colors.white;
+const Color appUiThemeColor = Color.fromRGBO(13, 152, 186, 1);
+const Color appUiTextColor = Color.fromRGBO(169, 169, 169, 1);
